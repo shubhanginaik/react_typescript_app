46e1a197cef3ceb926a2557eddb9eda68051ac04
Shubhangi <shubhanginaik@BC-macair029s-MacBook-Air.local>
2022-03-04 12:11:41 +0200

    working fine

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
+
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import {Todo} from './todo.modal';
 
 const App: React.FC=() =>{
-  const todos= [{id:'t1', text: 'Walk 10km'}];
+  //const todos= useState([]);
+  //const todos= [{id:'t1', text: 'Walk 10km'}];
+  const [todos,setTodos]=useState<Todo[]>([]);
   const todoAddHandler = (text:string) => {
-    console.log(text);
+    setTodos((prevTodos)=>[
+      ...prevTodos,
+    {id: Math.random().toString(), text:text},]);
   }
 return (
 <div className="App"> 
 {/* A component that adds todos */} 
-<NewTodo />
+<NewTodo onAddTodo={todoAddHandler}/>
 <TodoList items={todos} />
 
   </div>
